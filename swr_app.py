e89fea354895077040a53901ee9f0ef6f493258d
T-Reign <sainttaelin@googlemail.com>
2026-05-11 22:31:51 +0100

    swr_app.py

--- swr_app.py
+++ swr_app.py
@@ -95,11 +95,10 @@ else:
         old_d = destination
         st.session_state.origin_val = old_d
         st.session_state.dest_val = old_o
-        st.session_state.origin_val = old_d
-        st.session_state.dest_val = old_o
         
         # Increment the counter to "kill" the old widgets and make new ones
-        st.session_state.flip_count += 1
+        st.session_state.flip_count += 3
+        st.cache_data.clear()
         st.rerun()
     
     # 7. Ticket Selection Logic
